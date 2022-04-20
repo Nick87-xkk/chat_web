830291cb44b16eaf20d8335340e62a25ddc74f11
Nick87-xkk <59241530+Nick87-xkk@users.noreply.github.com>
2022-04-20 11:50:12 +0800

    feat: 游客登录群聊消息状态缓存到sessionStorage中，刷新页面不丢失当前聊天记录，关闭标签销毁缓存

--- src/views/visitor/Visitor.vue
+++ src/views/visitor/Visitor.vue
@@ -14,9 +14,16 @@
         <el-button @click="joinChat">加入聊天</el-button>
       </el-row>
     </el-dialog>
+    <div class="visitor-room-info"><span>
+      游客聊天室
+    </span></div>
     <div class="visitor-chat-message">
-      <div class="visitor-message-item" v-for="item in visitorMessageList">
-        <span>{{ item.val }}</span>
+      <div :class="{'visitor-send':item.type === 'send','visitor-receive':item.type=='receive'}"
+           v-for="item in visitorMessageList">
+        <div>
+          <p>{{ item.nickname }}</p>
+          <span>{{ item.val }}</span>
+        </div>
         <el-avatar></el-avatar>
       </div>
     </div>
@@ -33,17 +40,17 @@
               v-for="it in emojiData"
               @click="inputEmoji(it)"
               :key="it.codes"
-              >{{ it.char }}</a
+            >{{ it.char }}</a
             >
           </div>
         </el-popover>
       </el-row>
       <textarea
-        v-model="visitorMessage"
+        v-model="message"
         rows="4"
         class="friend-chat-textarea"
         autofocus
-        @keyup.alt.enter=""
+        @keyup.alt.enter="visitorSendMessage"
       ></textarea>
       <el-row align="middle" justify="end">
         <span style="margin-right: 10px">Enter换行,Alt+Enter发送</span>
@@ -51,7 +58,7 @@
           style="margin: 0 5px 5px 0"
           type="primary"
           @click="visitorSendMessage"
-          >Send
+        >Send
         </el-button>
       </el-row>
     </div>
@@ -59,47 +66,79 @@
 </template>
 
 <script setup lang="ts">
-import { PictureRounded } from '@element-plus/icons-vue';
-import emojiData from '../../assets/emoji.json';
-import { inputEmoji } from '../../components/chat/chat';
+import { PictureRounded } from "@element-plus/icons-vue";
+import emojiData from "../../assets/emoji.json";
 // import {developmentURL} from '../../components/chat/FriendChat.vue'
-import socketIO from 'socket.io-client';
-import { onMounted, reactive, ref } from 'vue';
+import socketIO from "socket.io-client";
+import { onMounted, reactive, ref } from "vue";
+import { inputEmoji, message } from "../../components/chat/chat";
 
 const dialogFormVisible = ref(false);
-const visitorNickname = ref('');
+const visitorNickname = ref("");
 const joinChat = () => {
   !visitorNickname.value
     ? (dialogFormVisible.value = true)
     : (dialogFormVisible.value = false);
+  sessionStorage.setItem("nickname", visitorNickname.value);
 };
 onMounted(() => {
-  dialogFormVisible.value = true;
-});
-// 游客登录，聊天室
-const socket = socketIO('ws://127.0.0.1:9892');
-const visitorMessage = ref('');
-const visitorMessageList:any = reactive([]);
-
-const visitorSendMessage = () => {
-  socket.emit('visitor', { nickname:visitorNickname.value,val:visitorMessage.value });
-  if (visitorMessage.value){
-    visitorMessageList.push({ type:'send',val:visitorMessage.value });
+  let storageNickname = sessionStorage.getItem("nickname");
+  let storageMessage:any =  sessionStorage.getItem("visitorMessage")
+  if (storageNickname) {
+    visitorNickname.value = storageNickname;
+    if (storageMessage){
+      storageMessage = JSON.parse(storageMessage);
+     storageMessage.forEach((x: any)=>{
+       visitorMessageList.push(x)
+     })
+    }
     setTimeout(() => {
       document
-        .querySelector('.friend-chat-textarea')!
+        .querySelector(".visitor-chat-message")!
         .scrollTo(
           0,
-          document.querySelector('.friend-chat-textarea')!.scrollHeight
+          document.querySelector(".visitor-chat-message")!.scrollHeight
         );
-      visitorMessage.value = '';
     }, 0);
+  } else {
+    dialogFormVisible.value = true;
+  }
+});
+// 游客登录，聊天室
+const socket = socketIO("ws://127.0.0.1:9892");
+const visitorMessage = ref("");
+const visitorMessageList: any = reactive([]);
+// 发送消息
+const visitorSendMessage = () => {
+  socket.emit("visitor", { nickname: visitorNickname.value, val: message.value });
+  if (message.value) {
+    visitorMessageList.push({ type: "send", val: message.value, nickname: visitorNickname.value });
+    setTimeout(() => {
+      document
+        .querySelector(".visitor-chat-message")!
+        .scrollTo(
+          0,
+          document.querySelector(".visitor-chat-message")!.scrollHeight
+        );
+      message.value = "";
+    }, 0);
+
+    sessionStorage.setItem('visitorMessage',JSON.stringify(visitorMessageList))
   }
 };
-
-socket.on('visitor', (msg) => {
+// 接收消息
+socket.on("visitor", (msg) => {
   if (msg) {
-    visitorMessageList.push({type:"receive",val:msg.val});
+    visitorMessageList.push({ type: "receive", val: msg.val, nickname: msg.nickname });
+    setTimeout(() => {
+      document
+        .querySelector(".visitor-chat-message")!
+        .scrollTo(
+          0,
+          document.querySelector(".visitor-chat-message")!.scrollHeight
+        );
+    }, 0);
+    sessionStorage.setItem('visitorMessage',JSON.stringify(visitorMessageList))
   }
 });
 
@@ -111,6 +150,7 @@ socket.on('visitor', (msg) => {
   justify-content: center;
   align-items: center;
   flex-direction: column;
+
 }
 
 .visitor-chat-message {
@@ -118,7 +158,9 @@ socket.on('visitor', (msg) => {
   height: 60%;
   box-shadow: 0 0 0 1px;
   backdrop-filter: blur(10px);
-  -webkit-backdrop-filter: blur(10px);
+  -webkit-backdrop-filter: blur(1px);
+  padding: 10px 5px 10px 5px;
+  overflow: auto;
 }
 
 .visitor-chat-input {
@@ -127,6 +169,7 @@ socket.on('visitor', (msg) => {
   width: 50%;
   height: 20%;
   box-shadow: 0 0 0 1px;
+  padding: 10px 5px 10px 5px;
   backdrop-filter: blur(10px);
   -webkit-backdrop-filter: blur(10px);
 }
@@ -173,6 +216,75 @@ socket.on('visitor', (msg) => {
 
   a:active {
     text-decoration: none;
+  }
+}
+
+.visitor-room-info {
+  width: 50%;
+  box-shadow: 0 0 0 1px;
+  padding: 10px 5px 10px 5px;
+  backdrop-filter: blur(10px);
+  -webkit-backdrop-filter: blur(10px);
+
+  span {
+    color: black;
+    font-size: 20px;
+  }
+}
+
+.visitor-send {
+  display: flex;
+  margin-top: 20px;
+  justify-content:end;
+  div {
+    max-width: 50%;
+    padding:0 5px 5px 5px;
+    p {
+      font-size: 10px;
+      text-align: right;
+      color: #000;
+      padding-bottom: 5px;
+    }
+
+    span {
+      float: right;
+      border-radius: 5px;
+      padding: 10px;
+      background-color: yellow;
+      word-wrap: break-word;
+      word-break: break-all;
+      white-space: pre-line;
+      text-align: left;
+    }
+  }
+}
+
+.visitor-receive {
+  display: flex;
+  flex-direction: row-reverse;
+  justify-content: start;
+  margin-top: 20px;
+
+  div {
+    max-width: 50%;
+    padding:0 5px 5px 5px;
+    p {
+      font-size: 10px;
+      text-align: left;
+      color: #000;
+      padding-bottom: 5px;
+    }
+
+    span {
+      float: left;
+      border-radius: 5px;
+      padding: 10px;
+      background-color: #00ff19;
+      word-wrap: break-word;
+      word-break: break-all;
+      white-space: pre-line;
+      text-align: left;
+    }
   }
 }
 </style>
---
+++ src/views/visitor/VisitorBaseInfo.vue
@@ -0,0 +1,11 @@
+<template>
+
+</template>
+
+<script setup lang="ts">
+// 填写游客基础信息，保存到sessionStorage中
+</script>
+
+<style scoped lang="scss">
+
+</style>
